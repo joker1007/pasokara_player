308e016e3e05eccddb7a5f26da93252929490e61
joker1007 <kakyoin.hierophant@gmail.com>
2009-11-07 18:14:03 +0900

    add clearfix & robots.txt

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -45,12 +45,14 @@ module ApplicationHelper
     %Q{
       <div id="info-list-#{entity.id}" class="info_list">
         <h3>動画情報</h3>
-        <div class="thumb">#{image_tag(url_for(:controller => "pasokara", :action => "thumb", :id => entity.id), :size => "160x120")}</div>
-        <div class="nico_info"><span class="info_key">ニコニコID:</span><span class="info_value">#{link_to(entity.nico_name, "http://www.nicovideo.jp/watch/" + entity.nico_name) if entity.nico_name}</span></div>
-        <div class="nico_info"><span class="info_key">投稿日時:</span><span class="info_value">#{h entity.nico_post}</span></div>
-        <div class="nico_info"><span class="info_key">再生数:</span><span class="info_value">#{number_with_delimiter(entity.nico_view_counter)}</span></div>
-        <div class="nico_info"><span class="info_key">コメント数:</span><span class="info_value">#{number_with_delimiter(entity.nico_comment_num)}</span></div>
-        <div class="nico_info"><span class="info_key">マイリスト数:</span><span class="info_value">#{number_with_delimiter(entity.nico_mylist_counter)}</span></div>
+        <div class="thumb clearfix">#{image_tag(url_for(:controller => "pasokara", :action => "thumb", :id => entity.id), :size => "160x120")}</div>
+        <div class="nico_info clearfix">
+          <span class="info_key">ニコニコID:</span><span class="info_value">#{link_to(entity.nico_name, "http://www.nicovideo.jp/watch/" + entity.nico_name) if entity.nico_name}</span><br />
+          <span class="info_key">投稿日時:</span><span class="info_value">#{h entity.nico_post}</span><br />
+          <span class="info_key">再生数:</span><span class="info_value">#{number_with_delimiter(entity.nico_view_counter)}</span><br />
+          <span class="info_key">コメント数:</span><span class="info_value">#{number_with_delimiter(entity.nico_comment_num)}</span><br />
+          <span class="info_key">マイリスト数:</span><span class="info_value">#{number_with_delimiter(entity.nico_mylist_counter)}</span><br />
+        </div>
       </div>
     }
   end
