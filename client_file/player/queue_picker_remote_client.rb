ccd512cf709e5b1620a14928eb4a53715138c587
joker1007 <kakyoin.hierophant@gmail.com>
2009-11-08 14:27:37 +0900

    some fix

--- client_file/player/queue_picker_remote_client.rb
+++ client_file/player/queue_picker_remote_client.rb
@@ -13,7 +13,6 @@ class QueuePickerClient
 
   def initialize
     @remote_queue_picker = DRbObject.new_with_uri("druby://" + ARGV[0]) #キューピッカーサーバーに接続
-	@remote_queue_picker.add_online_computer(`hostname`.chomp)
     @player_thread = nil
     @playing = false
 	
