8bda2dd40a979457ee0f07435521d65768f031c5
joker1007 <kakyoin.hierophant@gmail.com>
2010-03-28 09:01:21 +0900

    spec/models/queued_file_spec.rbを修正

--- spec/models/queued_file_spec.rb
+++ spec/models/queued_file_spec.rb
@@ -5,6 +5,8 @@ require File.expand_path(File.dirname(__FILE__) + '/../db_error_helper')
 include DbErrorHelper
 
 describe QueuedFile do
+  fixtures :pasokara_files, :computers
+
   before(:each) do
     @valid_attributes = {
       :pasokara_file_id => 8340,
@@ -13,15 +15,28 @@ describe QueuedFile do
     @no_file_attributes = {
       :pasokara_file_id => 1111,
     }
+
+    @just_be_friends = pasokara_files(:just_be_friends)
   end
 
   it "適切なパラメーターで作成されること" do
     QueuedFile.create!(@valid_attributes)
   end
 
+  it "PasokaraFileをキューに入れられること" do
+    QueuedFile.enq(@just_be_friends)
+    QueuedFile.deq.pasokara_file.should == @just_be_friends
+  end
+
   it "存在しないファイルIDをキューに入れようとするとエラーになること" do
     test_for_db_error do
       QueuedFile.create!(@no_file_attributes)
     end
+  end
+  
+  it "dequeueされたときに、その曲の再生ログレコードが作成されること" do
+    QueuedFile.enq @just_be_friends
+    pasokara = QueuedFile.deq.pasokara_file
+    SingLog.find(:last).pasokara_file.should == pasokara
   end
 end
