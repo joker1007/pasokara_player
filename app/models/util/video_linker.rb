2c2f1dff7aa374e9c8de73dc115a2d23cc267be3
joker1007 <kakyoin.hierophant@gmail.com>
2010-10-21 01:03:31 +0900

    video tag test

--- app/models/util/video_linker.rb
+++ app/models/util/video_linker.rb
@@ -6,14 +6,16 @@ module Util
       pasokaras = PasokaraFile.all(:select => "id, fullpath")
       system("rm -rf #{File.join(RAILS_ROOT, "public", "video","*")}")
       pasokaras.each do |pasokara|
-        extname = File.extname(pasokara.fullpath)
-        if extname and extname =~ /mp4|flv/
-          subdir = ((pasokara.id / 1000) * 1000).to_s
-          unless File.exist?(File.join(RAILS_ROOT, "public", "video", subdir))
-            Dir.mkdir(File.join(RAILS_ROOT, "public", "video", subdir))
+        if pasokara.fullpath
+          extname = File.extname(pasokara.fullpath)
+          if extname =~ /mp4|flv/
+            subdir = ((pasokara.id / 1000) * 1000).to_s
+            unless File.exist?(File.join(RAILS_ROOT, "public", "video", subdir))
+              Dir.mkdir(File.join(RAILS_ROOT, "public", "video", subdir))
+            end
+            puts "#{pasokara.fullpath} => #{File.join(RAILS_ROOT, "public", "video", subdir, pasokara.id.to_s + extname)}"
+            system("ln -s \"#{pasokara.fullpath}\" #{File.join(RAILS_ROOT, "public", "video", subdir, pasokara.id.to_s + extname)}")
           end
-          puts "#{pasokara.fullpath} => #{File.join(RAILS_ROOT, "public", "video", subdir, pasokara.id.to_s + extname)}"
-          system("ln -s \"#{pasokara.fullpath}\" #{File.join(RAILS_ROOT, "public", "video", subdir, pasokara.id.to_s + extname)}")
         end
       end
     end
