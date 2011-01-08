231b31d698ffc6a8d83deae964f7e54fc0a1bdfa
joker1007 <kakyoin.hierophant@gmail.com>
2011-01-09 01:15:40 +0900

    spec修正

--- spec/views/sing_log/list.html.erb_spec.rb
+++ spec/views/sing_log/list.html.erb_spec.rb
@@ -1,12 +1,17 @@
 require 'spec_helper'
 
 describe "/sing_log/list" do
+  fixtures :sing_logs, :users, :pasokara_files
+
   before(:each) do
+    assigns[:sing_logs] = SingLog.paginate(:all, :order => "created_at desc", :per_page => 50, :page => params[:page])
     render 'sing_log/list'
   end
 
   #Delete this example and add some real ones or delete this file
-  it "should tell you where to find the file" do
-    response.should have_tag('p', %r[Find me in app/views/sing_log/list])
+  it "2つのログがリストに表示される" do
+    response.should have_tag('li')
+    response.should have_tag('li')
+    response.should have_tag('li')
   end
 end
