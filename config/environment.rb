4705c6cc3a1b708b0055ab7e11b3e49e3640cf50
joker1007 <kakyoin.hierophant@gmail.com>
2011-02-22 18:28:34 +0900

    gem整理

--- config/environment.rb
+++ config/environment.rb
@@ -21,7 +21,9 @@ Rails::Initializer.run do |config|
   config.gem "solr-ruby", :lib => "solr"
   config.gem "pit"
   config.gem "rvideo"
-  config.gem "twitter", :version => "=0.8.4"
+  config.gem "resque"
+  config.gem "memcache-client", :lib => "memcache"
+  config.gem "twitter", :version => "0.8.4"
   config.gem "greatseth-rvideo", :lib => "rvideo", :source => "http://gems.github.com"
   config.gem "cosine-active_record_encoding", :lib => "active_record_encoding", :source => "http://gems.github.com" if "".respond_to?(:force_encoding)
   # config.gem "aws-s3", :lib => "aws/s3"
