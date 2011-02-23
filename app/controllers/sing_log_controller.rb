c9cdcf80382c42ecf90a47b860363a4e13e36bf8
joker1007 <kakyoin.hierophant@gmail.com>
2011-02-23 21:07:03 +0900

    改行コード修正

--- app/controllers/sing_log_controller.rb
+++ app/controllers/sing_log_controller.rb
@@ -6,8 +6,4 @@ class SingLogController < ApplicationController
     @sing_logs = SingLog.paginate(:all, :order => "created_at desc", :per_page => 50, :page => params[:page])
   end
 
-  def show
-    @sing_log = SingLog.find(params[:id])
-  end
-
 end
